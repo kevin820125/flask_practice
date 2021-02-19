6d4a779038283b23827973266ea46d51263731e3
kevin Su <kevin820125100@gmail.com>
2021-02-18 20:33:38 -0800

    cal practice done

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,23 @@
-# Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+# @app.route("/add")
+# def addd():
+#     a = int(request.args['a'])
+#     b = int(request.args['b'])
+#     result = add(a,b)
+#     return str(result)
+operations ={
+    "add": add,
+    "sub": sub,
+    "mult": mult,
+    "div": div,
+}
+@app.route('/math/<op>')
+def cal(op):
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    result = operations[op](a,b)
+    return str(result)
